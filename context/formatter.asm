e06b422155882c2b3c898b3d9175e9f0c31ed76f
James Kefrey <james-kefrey@proton.me>
2025-04-11 18:10:24 +0000

    Create new file

---
+++ context/formatter.asm
@@ -0,0 +1,95 @@
+shr al,8
+add rcx,72
+rcr esi,2
+lodsw
+syscall
+shr ecx,7
+cmp ch,ch
+call [eax]
+rol cx,2
+sbb [rcx],rax
+rol ax,1
+idiv bh
+jmp [edi]
+rcr cx,6
+ror rdi,3
+adc [edx],ebx
+wait
+mul ah
+lodsb
+rcr edx,8
+lodsb
+rol bx,8
+cwd
+pause
+mul dl
+ret
+call [esi]
+sub dx,dx
+jmp [ebx]
+rdtsc
+idiv dh
+rol ebp,6
+rol ah,3
+and eax,8
+rol ah,7
+sbb rax,rbp
+ror cx,5
+mov eax,53
+shr bx,7
+sar dx,6
+shr eax,2
+imul ebp
+add [rsi],rbp
+imul ebp
+call [eax]
+ret
+jmp [eax]
+mul rdx
+sbb [ebp],eax
+or al,36
+rol esi,7
+mov rdx,rbp
+.Om406905359:
+nop
+adc cx,cx
+imul rdx
+sbb [rsi],rbp
+neg dh
+cli
+.y4f572505756:
+jmp [esi]
+add dh,25
+jmp [ebx]
+lodsb
+sar ebp,2
+stc
+sbb [eax],esi
+jmp [eax]
+jmp [edx]
+or bx,77
+lahf
+ror rax,1
+sal bx,7
+adc dl,40
+clc
+scasw
+sar rsi,5
+sub [rdx],rbp
+sbb rsi,39
+mov [eax],ecx
+jmp [ecx]
+ret
+rdtsc
+ret
+neg bx
+test [rsi],rbx
+sbb cx,99
+rcl edx,1
+lodsw
+scasb
+rol esi,8
+mul ax
+.AYvfAa359548210:
+sti
+shr bh,7
