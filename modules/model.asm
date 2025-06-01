53976cfdbd1f054b2477b0dfe714342a2e72516a
James Kefrey <james-kefrey@proton.me>
2025-06-01 07:27:10 +0000

    Create modules/model.asm

---
+++ modules/model.asm
@@ -0,0 +1,80 @@
+or dh,70
+imul rdx
+cmp [esi],eax
+jmp [ebp]
+nop
+ret
+test bl,34
+syscall
+wait
+rdtsc
+ret
+jmp [ecx]
+sal esi,1
+cmp [edx],eax
+nop
+ret
+idiv al
+mov ebx,ebp
+ror bx,5
+rcl bl,8
+sal cl,3
+rcr rbx,4
+adc bl,bl
+jmp [ebp]
+call [edi]
+jmp [edx]
+syscall
+hlt
+nop
+xor bl,83
+jmp [eax]
+clc
+shl ecx,2
+shr al,3
+call [edi]
+syscall
+sar ax,1
+sar rbp,3
+cwd
+test cx,cx
+pause
+neg ch
+hlt
+neg bh
+adc ax,3
+rcl edi,8
+.goj81240708436:
+rcr rdx,1
+cwd
+wait
+div eax
+ror cx,4
+test ebp,55
+wait
+lock
+sbb ecx,18
+sub ax,8
+ysBza46140753:
+rcr esi,7
+mul dx
+inc rsi
+shr ax,6
+imul rbx
+ret
+.CAF022321076:
+div bl
+sbb edi,ebx
+rcl bl,3
+rdtsc
+HfiYgf6d758563930:
+hlt
+sbb ebp,19
+imul edi
+sti
+jmp [edx]
+ret
+adc dh,59
+xor [rsi],rax
+cld
+rdtsc
