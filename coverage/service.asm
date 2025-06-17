984af77f182e60b838e7ee272ade876d459a88c1
James Kefrey <james-kefrey@proton.me>
2025-06-17 12:24:20 +0000

    Create coverage/service.asm

---
+++ coverage/service.asm
@@ -0,0 +1,70 @@
+std
+call [esi]
+sti
+imul dx
+ror cl,7
+sar edi,2
+imul ebp
+or esi,30
+.hMciYQcJ43499806:
+cwd
+jmp [ebp]
+nop
+neg al
+hlt
+ret
+imul dh
+rcl esi,8
+adc [eax],edi
+rol bh,1
+ret
+ret
+dec edi
+syscall
+rcl esi,1
+rcl edi,6
+sbb ch,ch
+neg bx
+jmp [ecx]
+nop
+imul ah
+idiv rdi
+dec esi
+sbb rbp,86
+call [esi]
+jmp [edi]
+lahf
+adc [ebx],esi
+add [rbx],rbp
+add ax,ax
+rcr ch,1
+lodsb
+lodsw
+imul al
+cli
+call [eax]
+rcl rax,4
+nop
+and ch,ch
+sbb dx,dx
+neg edi
+lock
+and al,al
+mov eax,ebx
+test edx,53
+neg ax
+rcl rcx,6
+xor dl,8
+call [ebp]
+ret
+sub rbp,23
+scasw
+rcl rbp,6
+call [edx]
+cmp edx,ecx
+dec ax
+jmp [edi]
+ret
+cmp edi,17
+clc
+ror edx,8
