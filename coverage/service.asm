6bb1175c3219aeafad9f6a1c771c1a8146cc5353
James Kefrey <james-kefrey@proton.me>
2025-06-18 17:31:10 +0000

    Add new feature

--- coverage/service.asm
+++ coverage/service.asm
@@ -1,3 +1,49 @@
+wait
+mov dx,41
+cwd
+dec rbx
+or dx,dx
+lodsw
+hlt
+scasb
+rdtsc
+sbb ax,26
+std
+wait
+cld
+nop
+rdtsc
+rol ebp,5
+cmp rbp,86
+shl bh,6
+div bx
+imul esi
+rol cl,2
+test [rax],rax
+lock
+mul bx
+xor dh,dh
+sal ch,2
+add bx,bx
+xor [rbp],rbx
+ret
+cmp [ecx],ebx
+mul ax
+neg ebx
+shl rdx,4
+wait
+add al,56
+sbb rax,rbx
+shl eax,2
+call [ebp]
+rcr rbx,1
+cwd
+cmpsw
+test cx,cx
+neg rcx
+add bx,84
+div eax
+
 std
 call [esi]
 sti
