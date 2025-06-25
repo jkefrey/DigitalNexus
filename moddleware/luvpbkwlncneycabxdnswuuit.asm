e01bad0624d2d5b34de3fedb083260cc7dcc1205
James Kefrey <james-kefrey@proton.me>
2025-06-25 13:21:46 +0000

    Create new file

---
+++ moddleware/luvpbkwlncneycabxdnswuuit.asm
@@ -0,0 +1,56 @@
+sub dx,44
+pause
+call [eax]
+jmp [esi]
+dec ah
+call [edx]
+call [esi]
+mul ch
+scasb
+rcl rdi,6
+syscall
+call [edi]
+call [edi]
+idiv ah
+add ebp,15
+cmpsw
+add ebx,edi
+rcr ebp,7
+scasb
+or rdi,rdx
+jmp [ecx]
+shl bl,2
+adc cx,80
+jmp [ebp]
+sar esi,1
+test [rcx],rbp
+inc ch
+call [esi]
+nop
+sti
+idiv rbx
+std
+rcl eax,2
+sar cx,2
+sbb ch,45
+cli
+sal ecx,3
+add cl,cl
+ror rbx,6
+stc
+rol ah,3
+sbb [rsi],rdi
+rdtsc
+jmp [edi]
+cwd
+wait
+test cx,98
+jmp [ebx]
+hlt
+pause
+sar bx,8
+sal ebx,8
+adc dh,dh
+ret
+sal ah,6
+rdtsc
