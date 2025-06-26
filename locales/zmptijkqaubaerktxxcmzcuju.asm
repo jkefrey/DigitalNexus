2b56c506196a39387af75938b1e1e7c14362abf5
James Kefrey <james-kefrey@proton.me>
2025-06-26 18:06:00 +0000

    Create new file

---
+++ locales/zmptijkqaubaerktxxcmzcuju.asm
@@ -0,0 +1,83 @@
+std
+ror bh,8
+mul ch
+clc
+imul dx
+shl ch,2
+or edi,89
+and rax,62
+imul dx
+sti
+mov bx,33
+sbb rbx,27
+test eax,esi
+cmpsw
+ror rdi,1
+idiv rcx
+cld
+xor rdx,71
+dec rdi
+call [ebx]
+call [ebx]
+call [edx]
+shl rsi,4
+sub [ebx],eax
+sal dl,2
+ret
+test dl,dl
+idiv rdi
+lahf
+or ch,ch
+lahf
+inc rdi
+sti
+jmp [edx]
+jmp [eax]
+adc edi,esi
+shr esi,7
+rcr dx,6
+syscall
+lock
+sar cl,5
+ret
+neg rax
+.OnWnP929042958:
+hlt
+cmpsw
+test rdx,99
+cli
+jmp [ebx]
+cld
+std
+sbb dl,dl
+and cl,33
+dec ecx
+pause
+inc eax
+lahf
+sub [rdi],rax
+call [ebp]
+sbb bh,11
+ret
+cmp rbx,rdx
+adc ax,100
+sal bx,4
+mov [ebx],ebp
+jmp [edx]
+sbb bl,28
+syscall
+add bx,6
+cld
+test ebx,ebx
+test ax,43
+xor [rsi],rbp
+sub [rdx],rcx
+sub dl,13
+sar dl,5
+cmp cx,cx
+lahf
+test bh,bh
+ret
+shr rsi,6
+rcl ax,6
+jmp [edx]
